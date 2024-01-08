6f950c0c759ec1ebbbf79f2d68db0fd74172d3a1
Irtaza Akram <irtaza.akram@arbisoft.com>
2024-01-08 12:46:40 +0500

    fix: bleach deprecation

--- lms/djangoapps/discussion/rest_api/render.py
+++ lms/djangoapps/discussion/rest_api/render.py
@@ -4,10 +4,10 @@ Content rendering functionality
 Note that this module is designed to imitate the front end behavior as
 implemented in Markdown.Sanitizer.js.
 """
-import bleach
+import nh3
 import markdown
 
-ALLOWED_TAGS = bleach.ALLOWED_TAGS | {
+ALLOWED_TAGS = nh3.ALLOWED_TAGS | {
     'br', 'dd', 'del', 'dl', 'dt', 'h1', 'h2', 'h3', 'h4', 'hr', 'img', 'kbd', 'p', 'pre', 's',
     'strike', 'sub', 'sup'
 }
@@ -25,13 +25,13 @@ def render_body(raw_body):
     This includes the following steps:
 
     * Convert Markdown to HTML
-    * Sanitise HTML using bleach
+    * Sanitise HTML using nh3
 
     Note that this does not prevent Markdown syntax inside a MathJax block from
     being processed, which the forums JavaScript code does.
     """
     rendered_html = markdown.markdown(raw_body)
-    sanitised_html = bleach.clean(
+    sanitised_html = nh3.clean(
         rendered_html,
         tags=ALLOWED_TAGS,
         protocols=ALLOWED_PROTOCOLS,
