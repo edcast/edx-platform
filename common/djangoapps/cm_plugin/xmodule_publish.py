9a54105df3ffd9b9152d82da5f5fb5c4bbe03487
Hariom Dubey <hdubey@csod.com>
2024-02-14 15:54:32 +0530

    EP-94346 : made the changes according to the python version change

--- common/djangoapps/cm_plugin/xmodule_publish.py
+++ common/djangoapps/cm_plugin/xmodule_publish.py
@@ -1,13 +1,10 @@
-import django.dispatch
-import yaml
-import urllib, httplib
-from django.dispatch import receiver
-from xmodule.modulestore.exceptions import ItemNotFoundError
-from cm_plugin.models import XModule_Metadata_Cache
-from djcelery import celery
-from celery.task import PeriodicTask
-from datetime import timedelta
 import requests
+from datetime import timedelta
+import yaml
+from xmodule.modulestore.exceptions import ItemNotFoundError
+from common.djangoapps.cm_plugin.models import XModule_Metadata_Cache
+from celery.task import PeriodicTask
+
 
 publishable_items = ['unit', 'video', 'quiz']
 headers = {"Content-type": "application/x-www-form-urlencoded"}
@@ -24,8 +21,9 @@ try:
 except:
     pass
 
+
 class ProcessCacheTask(PeriodicTask):
-    run_every = timedelta(minutes = 1)
+    run_every = timedelta(minutes=1)
     def run(self, **kwargs):
         self.process_cache()
 
@@ -34,29 +32,29 @@ class ProcessCacheTask(PeriodicTask):
         
         not_posted_items = XModule_Metadata_Cache.objects.filter(posted=False).filter(state="public")
         for item in not_posted_items.values():
-            item.cm_id = create_or_update_at_cm(item)
+            item.cm_id = ProcessCacheTask.create_or_update_at_cm(item)
             item.posted = False
             item.save()
 
         deleted_items = XModule_Metadata_Cache.objects.filter(state='deleted').filter(posted=True)
         for item in deleted_items.values():
-            delete_in_cm(item)
+            ProcessCacheTask.delete_at_cm(item.cm_id)
             item.cm_id = None
             item.posted = False
             item.save()
 
     @staticmethod
     def create_or_update_at_cm(params):
-        if params.cm_id is None:
-            cm_id = create_at_cm(params)
+        if params['cm_id'] is None:
+            cm_id = ProcessCacheTask.create_at_cm(params)
         else:
-            cm_id = update_at_cm(params)
+            cm_id = ProcessCacheTask.update_at_cm(params)
         return cm_id
 
     @staticmethod
     def convert_to_cm_params(params):
         post_params = {}
-        post_params['group_id'] =  params['course']
+        post_params['group_id'] = params['course']
         post_params['url'] = params['url']
         post_params['object_type'] = params['obj_type']
         post_params['title'] = params['title']
@@ -67,14 +65,14 @@ class ProcessCacheTask(PeriodicTask):
 
     @staticmethod
     def create_at_cm(params):
-        post_params = convert_to_cm_params(params)
-        req = requests.post(cm_url+'/lms_objects/',params=post_params,headers=headers)
+        post_params = ProcessCacheTask.convert_to_cm_params(params)
+        req = requests.post(cm_url+'/lms_objects/', params=post_params, headers=headers)
         return req.json()['id']
 
     @staticmethod
     def update_at_cm(params):
-        post_params = convert_to_cm_params(params)
-        req = requests.post(cm_url+'/lms_objects/'+params['cm_id'],params=post_params,headers=headers)
+        post_params = ProcessCacheTask.convert_to_cm_params(params)
+        req = requests.post(cm_url+'/lms_objects/'+params['cm_id'], params=post_params, headers=headers)
         return req.json()['id']
 
     @staticmethod
@@ -92,26 +90,26 @@ def process_xmodule(location, course, modulestore):
         obj = modulestore.get_item(location)
         obj_type = get_pretty_name(obj.__class__.__name__)
         if obj_type in publishable_items:
-            cache_metadata(obj,obj_type,location,course,modulestore)
+            cache_metadata(obj, obj_type, location, course, modulestore)
 
         # recurse over children
         for child in obj.get_children():
             process_xmodule(child.location, course, modulestore)
 
-    #TODO: Item deleted? Handle
+    # TODO: Item deleted? Handle
     except ItemNotFoundError:
         # send delete request
         delete_metadata(location, course, modulestore)
         pass
 
 def cache_metadata(obj, obj_type, location, course, modulestore):
-    from contentstore.utils import get_lms_link_for_item
-    from contentstore.utils import compute_unit_state, UnitState
+    from cms.djangoapps.contentstore.utils import get_lms_link_for_item
+    from cms.djangoapps.contentstore.utils import compute_unit_state, UnitState
     params = {}
     params['course'] = str(course.location)
     params['start'] = obj.start
     params['due'] = obj.due
-    params['url'] = get_lms_link_for_item(location,course_id = course.location.course_id, preview=False)
+    params['url'] = get_lms_link_for_item(location, course_id=course.location.course_id, preview=False)
     params['state'] = str(compute_unit_state(obj))
     params['obj_type'] = obj_type
     params['title'] = obj.display_name
@@ -121,7 +119,7 @@ def cache_metadata(obj, obj_type, location, course, modulestore):
     # first see if exists already
     try:
         cache = XModule_Metadata_Cache.objects.get(url=params['url'])
-        for attr, value in params.iteritems():
+        for attr, value in params.items():
             setattr(cache, attr, value)
     except XModule_Metadata_Cache.DoesNotExist:
         cache = XModule_Metadata_Cache(**params)
@@ -130,8 +128,8 @@ def cache_metadata(obj, obj_type, location, course, modulestore):
     return
 
 def delete_metadata(location, course, modulestore):
-    from contentstore.utils import get_lms_link_for_item
-    obj_url = get_lms_link_for_item(location,course_id = course.location.course_id, preview=False)
+    from cms.djangoapps.contentstore.utils import get_lms_link_for_item
+    obj_url = get_lms_link_for_item(location, course_id=course.location.course_id, preview=False)
     try:
         existing_item = XModule_Metadata_Cache.objects.get(url=obj_url)
         existing_item.state = 'deleted'
